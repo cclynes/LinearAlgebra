f8d83fe1b143ab5cdd03dd95faf4d7050995ec89
cclynes <clynes.cw@gmail.com>
2024-07-27 23:22:22 -0400

    clarify namespace scopes

---
+++ examples/vector_examples.cpp
@@ -0,0 +1,6 @@
+#include "../include/LinearAlgebra.hpp"
+
+using namespace LinearAlgebra;
+namespace la = LinearAlgebra;
+
+la::Vector<double> defaultVec;
--- include/LinearAlgebra.hpp
+++ include/LinearAlgebra.hpp
@@ -1,12 +1,8 @@
 #ifndef LINEARALGEBRA_HPP
 #define LINEARALGEBRA_HPP
 
-namespace LinearAlgebra {
-
 #include "Vector.hpp"
 #include "Matrix.hpp"
 #include "Utility.hpp"
-
-} // namespace LinearAlgebra
 
 #endif // LINEARALGEBRA_HPP
